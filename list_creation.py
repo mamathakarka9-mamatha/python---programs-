0e5e38f611cc7dbf8d965e316f2d9d3488b04406
mamathakarka9-mamatha <mamathakarka9@gmail.com>
2026-07-07 18:03:17 +0530

    Add files via upload

---
+++ list_creation.py
@@ -0,0 +1,3 @@
+#creating list in dymanic code 
+list = list(map(int,input("enter the number").split()))#taking input as a string splits it and mapping it by using type casting converting into integer and converting it into list
+print(list)

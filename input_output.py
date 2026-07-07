0cf61e672d0cab0340f00aded6b3401d97d6ac2c
mamathakarka9-mamatha <mamathakarka9@gmail.com>
2026-07-07 11:58:45 +0530

    Add files via upload

---
+++ input_output.py
@@ -0,0 +1,8 @@
+#from user taking name, phone number, e-mail id as input and generating it as a output
+name = input("Please! Enter your name:")#input and output is considered as a string
+phone_number = int(input("Please! Enter your phone number"))#input is considered as a string so by using type casting output is considered as a integer
+Email_id = input("Please! Enter your e-mail_id")
+
+print("Name of the person:", name)
+print("Phone Number of the person:", phone_number)
+print("E-mail ID of the person:", Email_id)

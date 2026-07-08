56dc087a471ca154d2f7a3c4a8b1f31647a24e80
mamathakarka9-mamatha <mamathakarka9@gmail.com>
2026-07-08 10:31:51 +0530

    Add files via upload

---
+++ division.py
@@ -0,0 +1,5 @@
+#dividing two numbers 
+a = int(input("enter your first number: ")) #by using type casting converting a input string into integer
+b = int(input("enter your second number: "))#by using type casting converting b input string into integer
+print(a//b)#floor division
+print(a/b)#normal division

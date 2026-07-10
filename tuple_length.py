adc3a422fa4d17a982e0617397a1beb65ea0b256
mamathakarka9-mamatha <mamathakarka9@gmail.com>
2026-07-10 19:31:12 +0530

    Add files via upload

---
+++ tuple_length.py
@@ -0,0 +1,3 @@
+
+name = tuple(map(int, input("enter the tuple elements: ").split()))#creating a tuple from user input
+print("The length of the tuple is: ", len(name))#displaying the length of the tuple

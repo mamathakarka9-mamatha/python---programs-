375401579051eba394e1c57a1c23abae6836779b
mamathakarka9-mamatha <mamathakarka9@gmail.com>
2026-07-10 19:29:46 +0530

    Add files via upload

---
+++ tuple_creation.py
@@ -0,0 +1,3 @@
+#creating a tuple from user input and displaying it
+name = tuple(map(int, input("enter the tuple elements: ").split()))#creating a tuple from user input
+print("The tuple is: ", name)#displaying the tuple

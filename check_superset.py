ed07d14c5ec1286df55f745e4c2103f54ad10ceb
mamathakarka9-mamatha <mamathakarka9@gmail.com>
2026-07-18 18:25:25 +0530

    Add files via upload

---
+++ check_superset.py
@@ -0,0 +1,9 @@
+#checking whether two sets s1 and s2 are superset or not
+s1 = set(input("Enter your set1 elements:").split())#creating set s1 from user input
+s2 = set(input("Enter your set2 elements:").split())#creating set s2 from user input
+print("The set s1:", s1)#displaying set s1
+print("The set s2:", s2)#displaying set s2
+result1 = s1.issuperset(s2)#checking whether set s1 is superset of s2 or not
+print("The s1 superset of s2:", result1)#displaying result
+result2 = s2.issuperset(s1)#checking whether set s2 is superset of s1 or not
+print("The s2 superset of s1:", result2)#displaying result
